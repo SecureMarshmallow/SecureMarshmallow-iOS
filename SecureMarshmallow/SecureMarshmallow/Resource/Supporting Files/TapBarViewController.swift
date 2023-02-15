1803e9d23374f2292cfa32032369b8c17f5a771e
junha <goodjunha@gmail.com>
2023-02-15 17:03:53 +0900

    🫧 ::custom tabbar  추가

---
+++ SecureMarshmallow/SecureMarshmallow/Resource/Supporting Files/TapBarViewController.swift
@@ -0,0 +1,70 @@
+//
+//  TapBarViewController.swift
+//  SecureMarshmallow
+//
+//  Created by 박준하 on 2023/02/15.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class TapBarViewController: UITabBarController {
+    private lazy var homeViewController: UINavigationController = {
+        let viewController = ErrorViewController()
+        let tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        let navigationView = UINavigationController(rootViewController: viewController)
+        return navigationView
+    }()
+    
+    private lazy var passwordViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "비밀", image: UIImage(systemName: "lock.fill"), tag: 1)
+        viewController.tabBarItem = tabBarItem
+        let navigationView = UINavigationController(rootViewController: viewController)
+        return navigationView
+    }()
+    
+    private lazy var pictureViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "사진", image: UIImage(systemName: "photo.fill"), tag: 2)
+        viewController.tabBarItem = tabBarItem
+        let navigationView = UINavigationController(rootViewController: viewController)
+        return navigationView
+    }()
+    
+    private lazy var userViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "유저", image: UIImage(systemName: "person.fill"), tag: 3)
+        viewController.tabBarItem = tabBarItem
+        let navigationView = UINavigationController(rootViewController: viewController)
+        return navigationView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers = [homeViewController, passwordViewController, pictureViewController, userViewController]
+        configureTabBar()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        var tabFrame = self.tabBar.frame
+        tabFrame.size.height = 90
+        tabFrame.origin.y = self.view.frame.size.height - 90
+        self.tabBar.frame = tabFrame
+    }
+}
+
+extension UITabBarController {
+    
+    func configureTabBar() {
+        self.tabBar.tintColor = .label
+        self.tabBar.layer.cornerRadius = 20
+        self.tabBar.layer.masksToBounds = true
+        self.tabBar.layer.backgroundColor = UIColor.systemBackground.cgColor
+        self.tabBar.layer.borderWidth = 0.4
+    }
+    
+}
