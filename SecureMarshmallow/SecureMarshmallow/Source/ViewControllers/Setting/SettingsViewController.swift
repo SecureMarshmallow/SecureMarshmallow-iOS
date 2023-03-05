66703e73839b39522e463bbc7652f42912623093
junha <goodjunha@gmail.com>
2023-03-06 02:07:33 +0900

    🫧:: cell의 간격 수정

--- SecureMarshmallow/SecureMarshmallow/Source/ViewControllers/Setting/SettingsViewController.swift
+++ SecureMarshmallow/SecureMarshmallow/Source/ViewControllers/Setting/SettingsViewController.swift
@@ -29,7 +29,6 @@ class SettingsViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(SettingsCell.self, forCellReuseIdentifier: SettingsCell.reuseIdentifier)
-        tableView.sectionHeaderHeight = 10 // 간격 설정
         tableView.tableFooterView = UIView()
         tableView.separatorStyle = .none
         tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
@@ -45,51 +44,73 @@ class SettingsViewController: UIViewController {
 //                        SettingsItem(type: .notification, hasSwitch: true, switchState: true)]
         let section1 = [SettingsItem(type: .gmailInformation),
                         SettingsItem(type: .idInformation)]
-        let section2 = [SettingsItem(type: .intrusionInformation),
-                        SettingsItem(type: .appPassword),
-                        SettingsItem(type: .changeAppIcon)]
+        let section2 = [SettingsItem(type: .appPassword),
+                        SettingsItem(type: .intrusionInformation),
+                        SettingsItem(type: .appTracking)]
+        
+        let section3 = [SettingsItem(type: .changeAppIcon),
+                        SettingsItem(type: .changeAppTheme)]
+        
+        let section4 = [SettingsItem(type: .help),
+                        SettingsItem(type: .bugReport)]
+        
+        let section5 = [SettingsItem(type: .feedback)]
+        
+        let section6 = [SettingsItem(type: .appShare),
+                        SettingsItem(type: .privacyPolicy),
+                        SettingsItem(type: .termsofUse)]
+        
+        let section7 = [SettingsItem(type: .howToUse),
+                        SettingsItem(type: .developerIformation)]
 //        let section3 = [SettingsItem(type: .appTracking)]
-        settingsItems = [section1, section2]
+        settingsItems = [section1, section2, section3, section4, section5, section6, section7]
     }
 }
 
 // MARK: - UITableViewDataSource
 
 extension SettingsViewController: UITableViewDataSource {
+    
+    // 각 섹션의 footer 뷰 설정
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    //색션의 개수
     func numberOfSections(in tableView: UITableView) -> Int {
         return settingsItems.count
     }
     
+    //cell의 개수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return settingsItems[section].count
     }
     
+    //cell의 특징
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: SettingsCell.reuseIdentifier, for: indexPath) as! SettingsCell
         let item = settingsItems[indexPath.section][indexPath.row]
         cell.configure(with: item)
         return cell
     }
-    
-//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        let headerView = UIView()
-//        headerView.backgroundColor = .systemGroupedBackground
-//
-//        let titleLabel = UILabel()
-//        titleLabel.font = .systemFont(ofSize: 22, weight: .medium)
-//        titleLabel.text = section == 0 ? "ACCOUNT" : (section == 1 ? "HELP" : "LOG OUT")
-//        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-//        headerView.addSubview(titleLabel)
-//        titleLabel.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 16).isActive = true
-//        titleLabel.centerYAnchor.constraint(equalTo: headerView.centerYAnchor).isActive = true
-//
-//        return headerView
-//    }
+
 }
 
 // MARK: - UITableViewDelegate
 
 extension SettingsViewController: UITableViewDelegate {
+    
+    //cell의 간격
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+    // 셀 높이 설정
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    //cell을 클릭했을 경우
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -113,6 +134,24 @@ extension SettingsViewController: UITableViewDelegate {
             break
         case .changeAppIcon:
             print("앱 아이콘 변경")
+        case .changeAppTheme:
+            print("앱 테마 변경 클릭")
+        case .help:
+            print("지원")
+        case .bugReport:
+            print("버그 제보")
+        case .feedback:
+            print("피드백")
+        case .appShare:
+            print("앱 공유")
+        case .privacyPolicy:
+            print("개인정보 처리 방침")
+        case .termsofUse:
+            print("이용 약관 클릭")
+        case .howToUse:
+            print("사용 방법")
+        case .developerIformation:
+            print("개발자 정보")
         }
     }
 }
