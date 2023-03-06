364d802f236eade9e12da54919d995953e278ca1
junha <goodjunha@gmail.com>
2023-03-06 09:15:43 +0900

    🌊:: CompleteViewController 주석추가

--- SecureMarshmallow/SecureMarshmallow/Source/ViewControllers/CharacterCustom/CompleteViewController.swift
+++ SecureMarshmallow/SecureMarshmallow/Source/ViewControllers/CharacterCustom/CompleteViewController.swift
@@ -1,30 +1,28 @@
-//
-//  completeViewController.swift
-//  SecureMarshmallow
-//
-//  Created by 박준하 on 2023/02/07.
-//
-
 import UIKit
 import SnapKit
 import Then
 
+//캐릭터 선택을 맞히고 완료된 view를 의미 합니다.
 class CompleteViewController: UIViewController {
     
+    //유저가 선택한 바탕을 선언합니다.
     private lazy var backgroundView = UIView().then {
         $0.layer.cornerRadius = 100 * (view.frame.width / 430)
     }
     
+    //유저가 선택한 캐릭터를 보여줍니다.
     private lazy var emojiCharacter = UILabel().then {
         $0.font = .systemFont(ofSize: 150.0, weight: .black)
         $0.text = ""
     }
     
+    //말 그대로 환영합니다. title이 나오도록 선언되었습니다.
     private lazy var welcomeTitle = UILabel().then {
         $0.font = .systemFont(ofSize: 24.0, weight: .semibold)
         $0.text = "%s님 환영합니다"
     }
     
+    //proFile이 처음으로 갱신된 시간을 가져옵니다.
     private lazy var profileUpdateMessage = UILabel().then {
         $0.font = .systemFont(ofSize: 14.0, weight: .regular)
         $0.text = "2023.02.07 프로필 업데이트"
@@ -32,6 +30,7 @@ class CompleteViewController: UIViewController {
 
     }
     
+    //완료 버튼을 선언합니다.
     private lazy var completeButton = UIButton().then {
         $0.setTitle("완료", for: .normal)
         $0.setTitleColor(UIColor(ciColor: .black), for: .normal)
@@ -41,6 +40,7 @@ class CompleteViewController: UIViewController {
         $0.layer.borderWidth = 0.0
     }
     
+    // view의 생명주기에서 뷰가 나타나기 직전 호출될 떄 호출됩니다.
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
@@ -48,6 +48,7 @@ class CompleteViewController: UIViewController {
         emojiCharacter.text = CharacterSelectionViewController.userEmoji
     }
     
+    //view의 생명주기에서 뷰의 메모리가 로드된 후 호출됩니다.
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -55,6 +56,7 @@ class CompleteViewController: UIViewController {
         layout()
     }
     
+    //layout을 선언합니다.
     func layout() {
         [
             backgroundView,
